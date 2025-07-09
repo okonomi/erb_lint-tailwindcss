c86375188069e77e5f47f74b3949d00e3f681891
okonomi <okonomi@oknm.jp>
2025-07-09 22:43:52 +0900

    initial

---
+++ lib/erb_lint/tailwindcss.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tailwindcss/version"
+
+module ErbLint
+  module Tailwindcss
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/erb_lint/tailwindcss/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ErbLint
+  module Tailwindcss
+    VERSION = "0.1.0"
+  end
+end
